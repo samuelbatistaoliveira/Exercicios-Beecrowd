2810dca46c7e699ce8fcb953594d1f6173e5e88d
SamuelBxl <samuelbbatista32@gmail.com>
2025-09-11 18:18:43 -0300

    teste

---
+++ Iniciante/1008.py
@@ -0,0 +1,7 @@
+numerofuncionarios = int(input())
+horastrabalhadas = int(input())
+valorporhora = float(input())
+
+salario = horastrabalhadas * valorporhora
+print(f"NUMBER = {numerofuncionarios}")
+print(f"SALARY = U$ {salario:.2f}")
