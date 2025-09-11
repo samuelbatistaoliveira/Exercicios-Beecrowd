cf2de748f3e6802ff7b6185bb4e53162c42b730c
SamuelBxl <samuelbbatista32@gmail.com>
2025-09-11 06:45:23 -0300

    teste

---
+++ Iniciante/1002.py
@@ -0,0 +1,5 @@
+raio = float(input())
+Pi = 3.14159
+
+Area = Pi * (raio *2)
+print("A=%.4f"%Area)
---
+++ Iniciante/1005.py
@@ -0,0 +1,5 @@
+A = float(input())
+B = float(input())
+
+Media = (A * 3.5 + B * 7.5) / 11
+print(f"MEDIA = {Media:.5f}")
