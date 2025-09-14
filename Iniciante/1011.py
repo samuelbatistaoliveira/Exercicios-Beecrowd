6950c868f299347fb61a0f0f208066141afb1a53
SamuelBxl <samuelbbatista32@gmail.com>
2025-09-13 21:32:43 -0300

    python

---
+++ Iniciante/1011.py
@@ -0,0 +1,6 @@
+import math
+
+raio = int(input())
+pI = 3.14159
+volume = (4/3) * pI * math.pow(raio,3)
+print(f"VOLUME = {volume:.3f}")
---
+++ Iniciante/1014.py
@@ -0,0 +1,4 @@
+distanciatotal = int(input())
+combustivel = float(input())
+totalgastoemLitro = distanciatotal / combustivel
+print(f"{totalgastoemLitro:.3f} km/l")
