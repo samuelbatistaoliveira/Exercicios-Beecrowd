95f6a9bd536baf6c1ae897f9c5a59f677d8b9c78
SamuelBxl <samuelbbatista32@gmail.com>
2025-09-29 16:50:32 -0300

    #

---
+++ Iniciante/1020.py
@@ -0,0 +1,11 @@
+N = int(input())
+
+anos = N // 365
+dias_restantes = N % 365
+
+meses = dias_restantes // 30
+dias = dias_restantes % 30
+
+print(f"{anos} ano(s)")
+print(f"{meses} mes(es)")
+print(f"{dias} dia(s)")
